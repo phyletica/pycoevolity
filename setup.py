ee3cc879fcf007ce820e260e6f0c3a633666d533
Jamie Oaks <joaks1@gmail.com>
2017-01-05 17:41:11 -0600

    Initial commit.

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+from sumcoevolity import __version__
+
+setup(name='sumcoevolity',
+      version=__version__,
+      description="Package for summarizing output of ecoevolity",
+      author="Jamie Oaks",
+      author_email="joaks1@gmail.com",
+      license="GPL",
+      packages=find_packages(),
+      zip_safe=False,
+      test_suite="sumcoevolity.test.get_unittest_suite",
+      # test_suite="sumcoevolity.test",
+      # test_loader="unittest:TestLoader",
+      install_requires=[
+          # 'matplotlib'
+      ],
+      )
