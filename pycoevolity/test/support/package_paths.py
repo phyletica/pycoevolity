8a6658604c8095c5b981254c525661f082ff2050
Jamie R. Oaks <joaks1@gmail.com>
2017-11-02 17:45:04 -0500

    Removing import of script dir in test support.

--- pycoevolity/test/support/package_paths.py
+++ pycoevolity/test/support/package_paths.py
@@ -2,8 +2,6 @@
 
 import os
 
-from pycoevolity import SCRIPTS_DIR
-
 LOCAL_DIR = os.path.abspath(os.path.dirname(__file__))
 TEST_DIR = os.path.dirname(LOCAL_DIR)
 TEST_DATA_DIR = os.path.join(TEST_DIR, "data")
@@ -14,9 +12,6 @@ def data_path(filename=""):
 
 def output_path(filename=""):
     return os.path.join(TEST_OUTPUT_DIR, filename)
-
-def script_path(filename=""):
-    return os.path.join(SCRIPTS_DIR, filename)
 
 def test_path(filename=""):
     return os.path.join(TEST_DIR, filename)
